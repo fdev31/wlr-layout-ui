53378144d120b54bec9b7faa69a72959e60d62ae
fdev31 <fdev31@gmail.com>
2023-04-16 20:06:26 +0200

    status bar + experimental snapping

--- src/wlr_layout_ui/__init__.py
+++ src/wlr_layout_ui/__init__.py
@@ -1,10 +1,16 @@
 #!/bin/env python
-import pygame
+import math
 import random
+
+import pygame
+
 from .base import Screen, displayInfo, load as loadDisplayInfo
 
+shared = {}
 
 class GuiScreen:
+
+
     def __init__(
         self,
         screen: Screen,
@@ -14,7 +20,7 @@ class GuiScreen:
         self.screen = screen
         self.rect = rect
         self.color = color
-        self.font = pygame.font.Font(None, 24)  # Change the font and size as desired
+        self.hovering = False
 
     def genColor(self):
         self.color = (
@@ -29,7 +35,7 @@ class GuiScreen:
         pygame.draw.rect(surface, (50, 50, 50), self.rect, width=3)
 
         # Render the screen uid as text
-        text = self.font.render(self.screen.uid, True, (0, 0, 0))
+        text = shared['font'].render(self.screen.uid, True, (0, 0, 0))
 
         # Calculate the position to blit the text onto the rectangle
         text_rect = text.get_rect()
@@ -42,11 +48,24 @@ class GuiScreen:
             label = "N/A"
         else:
             label = "%dx%d" % (self.screen.mode.width, self.screen.mode.height)
-        text2 = self.font.render(label, True, (0, 0, 0))
+        text2 = shared['font'].render(label, True, (0, 0, 0))
         label_rect = text2.get_rect()
         label_rect.center = self.rect.center
         label_rect.y += text_rect.height + 10
         surface.blit(text2, label_rect)
+
+#       # Grey status bar with white text showing the screen name when mouse is hovering over it
+#        if self.hovering:
+#            status_bar_rect = pygame.Rect(self.rect.left, self.rect.bottom, self.rect.width, 20)
+#            pygame.draw.rect(surface, (150, 150, 150), status_bar_rect)
+#            status_text = shared['font'].render(self.screen.name, True, (255, 255, 255))
+#            status_text_rect = status_text.get_rect()
+#            status_text_rect.center = status_bar_rect.center
+#            surface.blit(status_text, status_text_rect)
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEMOTION:
+            self.hovering = self.rect.collidepoint(event.pos)
 
 
 def gui():
@@ -100,9 +119,11 @@ def gui():
 
     # Main loop for the Pygame application
     running = True
+    status_bar = ''
     while running:
         # Handle Pygame events
         for event in pygame.event.get():
+
             if event.type == pygame.QUIT:
                 running = False
 
@@ -115,6 +136,24 @@ def gui():
                         gui_screens.remove(gui_screen)
                         # Append it to the end of the list to ensure it is drawn on top of the others
                         gui_screens.append(gui_screen)
+                        break
+            elif event.type == pygame.MOUSEBUTTONUP:
+                for gui_screen in gui_screens:
+                    gui_screen.rect.x = round(gui_screen.rect.x/4) * 4
+                    gui_screen.rect.y = round(gui_screen.rect.y/4) * 4
+                # Check screen overlaps and correct the rectangles accordingly
+                active_screen = gui_screens[-1]
+                for gui_screen in gui_screens[:-1]:
+                    if gui_screen.rect.colliderect(active_screen.rect):
+                        # detect if active_screen is more north, west, est or west of gui_screen
+                        if active_screen.rect.x < gui_screen.rect.x:
+                            active_screen.rect.x = gui_screen.rect.x - active_screen.rect.width
+                        elif active_screen.rect.x > gui_screen.rect.x:
+                           active_screen.rect.x = gui_screen.rect.x + gui_screen.rect.width
+#                        if active_screen.rect.y < gui_screen.rect.y:
+#                            active_screen.rect.y = gui_screen.rect.y - active_screen.rect.height
+#                        elif active_screen.rect.y > gui_screen.rect.y:
+#                            active_screen.rect.y = gui_screen.rect.y + gui_screen.rect.height
                         break
 
             # Handle dragging events
@@ -130,8 +169,28 @@ def gui():
                         gui_screen.rect.y += event.rel[1]
                         break
 
+            # propagate to screens
+            for gui_screen in gui_screens:
+                gui_screen.handle_event(event)
+                if gui_screen.hovering:
+                    status_bar = gui_screen.screen.name
+
+
         # Draw the rectangles on the Pygame display surface
         display.fill((255, 255, 255))
+
+       # Grey status bar with white text showing the screen name when mouse is hovering over it
+        status_bar_rect = pygame.Rect(display.get_rect())
+        status_bar_rect.height = 20
+        status_bar_rect.y = display.get_rect().height - 20
+
+        pygame.draw.rect(display, (150, 150, 150), status_bar_rect)
+        status_text = shared['font'].render(status_bar, True, (255, 255, 255))
+        status_text_rect = status_text.get_rect()
+        status_text_rect.center = status_bar_rect.center
+        display.blit(status_text, status_text_rect)
+
+
         for gui_screen in gui_screens:
             gui_screen.draw(display)
 
@@ -143,6 +202,7 @@ def main():
     loadDisplayInfo()
     # Initialize Pygame
     pygame.init()
+    shared['font'] = pygame.font.Font(None, 24)  # Change the font and size as desired
     gui()
     pygame.quit()
 
