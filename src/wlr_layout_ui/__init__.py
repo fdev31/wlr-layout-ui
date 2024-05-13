13adfb49b0dbb5b2979033784c1ea5ab593abe45
Alessio Molinari <alessiomolinar@gmail.com>
2024-05-13 23:16:36 +0200

    feat: add wlrlui -m to automatically match profiles

--- src/wlr_layout_ui/__init__.py
+++ src/wlr_layout_ui/__init__.py
@@ -17,6 +17,31 @@ except ImportError:
     pass
 
 
+def apply_profile(profile):
+    load()
+    p_by_id = {p["uid"]: p for p in profile}
+    rects = [
+        (
+            Rect(-i["x"], i["y"], i["height"], i["width"])
+            if i.get("transform", 0) in (1, 3, 5, 7)
+            else Rect(-i["x"], i["y"], i["width"], i["height"])
+        )
+        for i in profile
+    ]
+
+    for i, di in enumerate(displayInfo):
+        di.active = p_by_id[di.uid]["active"]
+        di.transform = p_by_id[di.uid].get("transform", 0)
+        di.scale = p_by_id[di.uid].get("scale", 1.0)
+        if di.transform in (1, 3, 5, 7):
+            rects[i].width, rects[i].height = rects[i].height, rects[i].width
+
+    cmd = make_command(displayInfo, rects, not LEGACY)
+    time.sleep(0.5)
+    if os.system(cmd):
+        print("Failed applying the layout")
+
+
 def main():
     if len(sys.argv) > 1:
         from .profiles import load_profiles
@@ -26,16 +51,30 @@ def main():
             print("")
             for p in profiles.keys():
                 print(f" - {p}")
+        elif sys.argv[1] == "-m":
+            load()
+            current_uids = set(di.uid for di in displayInfo)
+            for key in sorted(profiles.keys()):
+                prof_uids = {p["uid"] for p in profiles[key]}
+                # check that the two sets have the same elements
+                if len(prof_uids - current_uids) == 0:
+                    print(f"Matched profile {key}. Applying it...")
+                    apply_profile(profiles[key])
+                    sys.exit(0)
+            print(f"No profile found: {sys.argv[1]}")
+            sys.exit(1)
+
         elif sys.argv[1][0] == "-":
             load()
             print(
                 """With no options, launches the GUI
 Options:
              -l : list profiles
+             -m : find a profile that matches the current displays layout, and apply it. 
+                  No-op if not found; will apply first in alphabetical order if multiple found.
  <profile name> : loads a profile
             """
             )
-
         else:
             reload_pre_commands()
             try:
@@ -43,28 +82,7 @@ Options:
             except KeyError:
                 print("No such profile: %s" % sys.argv[1])
                 raise SystemExit(1)
-            load()
-            p_by_id = {p["uid"]: p for p in profile}
-            rects = [
-                (
-                    Rect(-i["x"], i["y"], i["height"], i["width"])
-                    if i.get("transform", 0) in (1, 3, 5, 7)
-                    else Rect(-i["x"], i["y"], i["width"], i["height"])
-                )
-                for i in profile
-            ]
-
-            for i, di in enumerate(displayInfo):
-                di.active = p_by_id[di.uid]["active"]
-                di.transform = p_by_id[di.uid].get("transform", 0)
-                di.scale = p_by_id[di.uid].get("scale", 1.0)
-                if di.transform in (1, 3, 5, 7):
-                    rects[i].width, rects[i].height = rects[i].height, rects[i].width
-
-            cmd = make_command(displayInfo, rects, not LEGACY)
-            time.sleep(0.5)
-            if os.system(cmd):
-                print("Failed applying the layout")
+            apply_profile(profile)
         sys.exit(0)
     load()
     max_width = int(
